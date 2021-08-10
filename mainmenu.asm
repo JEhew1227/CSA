aff395bd0313c3a1ffc0e2d4ec5c3a048bcbbaf4
HeavensF33L <78343614+HeavensF33L@users.noreply.github.com>
2021-08-10 17:43:15 +0800

    Add files via upload

--- mainmenu.asm
+++ mainmenu.asm
@@ -1,6 +1,7 @@
 .model small
 .stack 100
 .data
+	NL DB 0DH, 0AH, "$"
 	line1 db '                       --------------------------$'
 	line2 db '                        Welcome to Happy Organic $'
 	line3 db '                       --------------------------$'
@@ -17,18 +18,48 @@
 	
 	prompt db '                          Enter Your Choice: $'
 	choice db ?
-	NL DB 0DH, 0AH, "$"
+	
+	
+	test1 db 'Hi $'
+	test2 db 'Hello $'
+	
+	
+	;sign up
+	promptSign db '                       Enter New User Name: $'
+	promptNewPasswrd db '                       Enter New Password: $'
+	
+	;login
+	promptLog db '                       Enter User Name: $'
+	promptpasswrd db '                       Enter Password: $' 
+	
+	
+	newUsrname db 31
+			   db ?
+			   db 31(?)
+	
+	newPasswrd db 31
+			   db ?
+			   db 31(?)
+	
+	
+	
 .code
 main proc
 	mov ax, @data
 	mov ds, ax
 
+	;new line
+	MOV AH, 09H
+	LEA DX, NL 
+	INT 21H
+	
+	;display logo
 	mov ah, 09h
 	lea dx, logo1 
 	int 21h
 	
 	MOV AH, 09H
-	LEA DX, NL ;new Line
+	LEA DX, NL 
 	INT 21H
 	
 	mov ah, 09h
@@ -36,7 +67,7 @@ main proc
 	int 21h
 	
 	MOV AH, 09H
-	LEA DX, NL ;new Line
+	LEA DX, NL 
 	INT 21H
 	
 	mov ah, 09h
@@ -44,7 +75,7 @@ main proc
 	int 21h
 	
 	MOV AH, 09H
-	LEA DX, NL ;new Line
+	LEA DX, NL 
 	INT 21H
 	 
 	mov ah, 09h
@@ -52,19 +83,19 @@ main proc
 	int 21h
 	
 	MOV AH, 09H
-	LEA DX, NL ;new Line
+	LEA DX, NL 
 	INT 21H
 	
 	mov ah, 09h
 	lea dx, logo5
 	int 21h
 	
+	;double new line
 	MOV AH, 09H	
-	LEA DX, NL ;new line
+	LEA DX, NL 
 	INT 21H
-	;Double New line 
 	MOV AH, 09H	
-	LEA DX, NL ;new line
+	LEA DX, NL 
 	INT 21H
 
 	mov ah, 09h
@@ -131,17 +162,82 @@ main proc
 	;Choice Entered and saved
 	mov ah, 01h	   
 	int 21h 
-	mov choice, al
 	
-	;new line
-	MOV AH, 09H	
-	LEA DX, NL 
-	INT 21H
+	cmp al, 31h
+	je signUp	
+	cmp al, 32h
+	je login
+	cmp al, 33h
+	je exit
+	
+	signUp: 
+		;new line
+		MOV AH, 09H	
+		LEA DX, NL 
+		INT 21H
+		
+		;prompt new username
+		mov ah, 09h
+		lea dx, promptSign
+		int 21h
+		
+		;new username input
+		mov ah, 0AH
+		mov dx, offset newUsrname
+		int 21h
+		
+		;new line
+		MOV AH, 09H	
+		LEA DX, NL 
+		INT 21H
+		
+		;prompt new password
+		mov ah, 09h
+		lea dx, promptNewPasswrd
+		int 21h
+		
+		;new password input
+		mov ah, 0AH
+		mov dx, offset newPasswrd
+		int 21h
+		
+		
+		jmp main
+		
+		
+	login:
+		;new line
+		MOV AH, 09H	
+		LEA DX, NL 
+		INT 21H
+	
+		;prompt new username
+		mov ah, 09h
+		lea dx, promptLog 
+		int 21h
+		
+		
+		
+		
+		
+		
+		
+		
+		jmp done
+	
+	exit: 
+		;newLine
+		mov ah, 09h
+		lea dx, NL
+		int 21h
+		
+		jmp done
+	
+	done: 
+		mov ah, 4ch	
+		int 21h
+		
 
 	
-
-
-	mov ah, 4ch	
-	int 21h
 main endp
 end main
