c0792edbf5505e8f6ba63749e0636177c9698f47
YLS1115 <68636344+YLS1115@users.noreply.github.com>
2021-08-11 23:37:13 +0800

    Update summary.asm
    Total need to change a lot the msg later i will change to str like qz's code

--- summary.asm
+++ summary.asm
@@ -1,59 +1,69 @@
 .model small
 .stack 100H
 .data
-	MSG1 DB "									Summary			   						 $"
+	MSG1 DB "List the products purchased? (Y or N)$"
 	MSG2 DB "*===========================================================================*$"
-	MSG3 DB "Product :$"
+	MSG3 DB "									Summary			   						  $"
 	MSG4 DB "*===========================================================================*$"
-	MSG5 DB "Total =$"
-	           ;STORE DIFFERENT CALACULATION
-	
+	MSG5 DB "Product :																	  $" 
+	MSG6 DB "*---------------------------------------------------------------------------*$"
+	MSG7 DB "SubTotal + Service Tax =													  $"
+	MSG8 DB "*---------------------------------------------------------------------------*$"
+	MSG9 DB "Total = 																	 *$"
+	MSG10 DB "*==========================================================================*$"
+	PROD1 DB ? ; ;Product user have selected 
+	Total DB ? ; ;Product user have selected 
+	Char1 DB ? ; ;To store decision
 .code
 main proc
 	mov ax, @data
 	mov ds, ax
 	
-	MOV AH,09H
-    LEA DX,MSG1
-    INT 21H
+	mov AH,09H
+    lea DX,MSG1
+    int 21H
 	
-	MOV dl, 10
-	MOV ah, 02h
-	INT 21h
+	mov ah,01H
+	int 21h
+	sub al,30H
+	mov Char1, al
+	int 21H
 	
-	MOV AH,09H
-    LEA DX,MSG2
-    INT 21H
+	if (Char1 = Y){
 	
-	MOV dl, 13
-	MOV ah, 02h
-	INT 21h
+	mov AH,09H
+    lea DX,MSG2
+    int 21H
 	
-	MOV AH,09H
-    LEA DX,MSG3
-    INT 21H
+	mov dl, 13
+	mov ah, 02h
+	int 21h
 	
-	MOV dl, 13
-	MOV ah, 02h
-	INT 21h
+	mov AH,09H
+    lea DX,MSG3
+    int 21H
+	
+	mov dl, 13
+	mov ah, 02h
+	int 21h
 
-	MOV AH,09H
-    LEA DX,MSG4
-    INT 21H
+	mov AH,09H
+    lea DX,MSG4
+    int 21H
 	
-	MOV dl, 13
-	MOV ah, 02h
-	INT 21h
+	mov dl, 13
+	mov ah, 02h
+	int 21h
 	
-	MOV AH,09H
-    LEA DX,MSG5
-    INT 21H
+	mov AH,09H
+    lea DX,MSG5
+    int 21H
 	
- ;TotalPrice
+	;TotalPrice ;this need to change a lot
 	
 	totalprice:
 	mov ah,9
-    Lea dx,m6
+    lea dx,m6
     int 21h 
             
     xor ax,ax
@@ -62,11 +72,11 @@ main proc
     call outdec
             
     mov ah,9
-    Lea dx,m13
+    lea dx,m13
     int 21h
             
     mov ah,9
-    Lea dx,m14
+    lea dx,m14
     int 21h
             
     mov ah,1
@@ -76,9 +86,15 @@ main proc
     je start
             
     mov ah,9
-    Lea dx,m7
+    lea dx,m7
     int 21h
-    
+	}
+	
+	if (Char1 = N) {
+	mov ah, 4CH
+	int 21h
+	}
+	
 	mov ah, 4CH
 	int 21h
 main endp	
