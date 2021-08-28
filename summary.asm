2381448ae9ebb6b1cd69a534ba4a61781b31806e
YLS1115 <68636344+YLS1115@users.noreply.github.com>
2021-08-28 13:14:15 +0800

    Update summary.asm

--- summary.asm
+++ summary.asm
@@ -1,119 +1,134 @@
 .model small
-.stack 100H
+.stack 100
 .data
-	MSG1 DB "									Receipt					 			      $"
-	MSG2 DB "*===========================================================================*$"
-	MSG3 DB "				  					Summary						              $"
-	MSG4 DB "*===========================================================================*$"
-	MSG5 DB "	Product :							      								  $" 
-	MSG6 DB "	Quantity:
-	MSG7 DB "*---------------------------------------------------------------------------*$"
-	MSG8 DB "	SubTotal + Service Tax :					     						  $"
-	MSG9 DB "*---------------------------------------------------------------------------*$"
-	MSG10 DB "	Total : 							     								 *$"
-	MSG11 DB "*==========================================================================*$"
-	MSG12 DB "				Thank you for the purchase! See you again!	     			  $"
+	MSG1 DB "|===========================================================|$"
+	MSG2 DB "|	                     Summary  			    |$"
+	MSG3 DB "|-----------------------------------------------------------|$"
+	MSG4 DB "|	              Products You Purchased  	            |$"
+	MSG5 DB "|===========================================================|$"
+	MSG6 DB "|Product :			                            |$" 
+	MSG7 DB "|Quantity:                                                  |$"
+	MSG8 DB "|-----------------------------------------------------------|$"
+	MSG9 DB "|SubTotal + Service Tax :			            |$"
+	MSG10 DB "|-----------------------------------------------------------|$"
+	MSG11 DB "|Total : 						    |$"
+	MSG12 DB "|===========================================================|$"
+	MSG13 DB "	Thank you for the purchase! See you again!	         $"
+    n_line DB 0AH,0DH,"$"
 
 .code
 main proc
 	mov ax, @data
 	mov ds, ax
+
+    ;CLEAR SCREEN
+    mov ax, 0003H
+    int 10H
 	
-	mov AH,09H
-    lea DX,MSG1
-    int 21H
-	
-	LEA DX,n_line
-    MOV AH,9
-    INT 21H 
-	
-	mov AH,09H
-    lea DX,MSG2
-    int 21H
+    L1:
+        mov AH,09H
+        lea DX,MSG1
+        int 21H
+        
+        LEA DX,n_line
+        MOV AH,9
+        INT 21H 
+        
+        mov AH,09H
+        lea DX,MSG2
+        int 21H
 
-	LEA DX,n_line
-    MOV AH,9
-    INT 21H 
-	
-	mov AH,09H
-    lea DX,MSG3
-    int 21H
-	
-	LEA DX,n_line
-    MOV AH,9
-    INT 21H 
+        LEA DX,n_line
+        MOV AH,9
+        INT 21H 
+        
+        mov AH,09H
+        lea DX,MSG3
+        int 21H
+        
+        LEA DX,n_line
+        MOV AH,9
+        INT 21H 
 
-	mov AH,09H
-    lea DX,MSG4
-    int 21H
-	
-	LEA DX,n_line
-    MOV AH,9
-    INT 21H 
-	
-	mov AH,09H
-    lea DX,MSG5
-    int 21H
+        mov AH,09H
+        lea DX,MSG4
+        int 21H
+        
+        LEA DX,n_line
+        MOV AH,9
+        INT 21H 
+        
+        mov AH,09H
+        lea DX,MSG5
+        int 21H
 
-	LEA DX,n_line
-    MOV AH,9
-    INT 21H 
+        LEA DX,n_line
+        MOV AH,9
+        INT 21H 
 
-	mov AH,09H
-    lea DX,MSG6
-    int 21H
+        mov AH,09H
+        lea DX,MSG6
+        int 21H
 
-	LEA DX,n_line
-    MOV AH,9
-    INT 21H 
+        LEA DX,n_line
+        MOV AH,9
+        INT 21H 
 
-	mov AH,09H
-    lea DX,MSG7
-    int 21H
+        mov AH,09H
+        lea DX,MSG7
+        int 21H
 
-	LEA DX,n_line
-    MOV AH,9
-    INT 21H 
+        LEA DX,n_line
+        MOV AH,9
+        INT 21H 
 
-	mov AH,09H
-    lea DX,MSG8
-    int 21H
+        mov AH,09H
+        lea DX,MSG8
+        int 21H
 
-	LEA DX,n_line
-    MOV AH,9
-    INT 21H 
+        LEA DX,n_line
+        MOV AH,9
+        INT 21H 
 
-	mov AH,09H
-    lea DX,MSG9
-    int 21H
+        mov AH,09H
+        lea DX,MSG9
+        int 21H
 
-	LEA DX,n_line
-    MOV AH,9
-    INT 21H 
+        LEA DX,n_line
+        MOV AH,9
+        INT 21H 
 
-	mov AH,09H
-    lea DX,MSG10
-    int 21H
+        mov AH,09H
+        lea DX,MSG10
+        int 21H
 
-	LEA DX,n_line
-    MOV AH,9
-    INT 21H 
+        LEA DX,n_line
+        MOV AH,9
+        INT 21H 
 
-	mov AH,09H
-    lea DX,MSG11
-    int 21H
+        mov AH,09H
+        lea DX,MSG11
+        int 21H
 
-	LEA DX,n_line
-    MOV AH,9
-    INT 21H 
-    
-    mov AH,09H
-    lea DX,MSG12
-    int 21H
+        LEA DX,n_line
+        MOV AH,9
+        INT 21H 
+        
+        mov AH,09H
+        lea DX,MSG12
+        int 21H
 
-	LEA DX,n_line
-    MOV AH,9
-    INT 21H 
+        LEA DX,n_line
+        MOV AH,9
+        INT 21H 
+
+        mov AH,09H
+        lea DX,MSG13
+        int 21H
+
+        LEA DX,n_line
+        MOV AH,9
+        INT 21H 
 	
 	mov ah, 4CH
 	int 21h
