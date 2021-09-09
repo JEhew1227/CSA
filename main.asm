540f962ccc5b5775c3b9c76fceae03b93e5864ff
QZhengC <86586890+QZhengC@users.noreply.github.com>
2021-09-09 12:49:53 +0800

    Add files via upload

--- main.asm
+++ main.asm
@@ -1,51 +1,26 @@
-; Color Codes (format: XY, X = background, Y = foreground)
-; 0 = Black
-; 1 = Blue
-; 2 = Green
-; 3 = Aqua
-; 4 = Red
-; 5 = Purple
-; 6 = Yellow
-; 7 = White
-; 8 = Gray
-; 9 = Light Blue
-CHANGE_COLOR macro colorCode, msg
-    mov ah, 09h
-    mov cx, 1000h
-    mov al, 20h
-    mov bl, colorCode
-    int 10h
-
-    lea dx, msg
-    mov ah, 09h
-    int 21h
-
-    mov ah, 09h
-    mov bl, 07h
-    int 10h
-endm
-
 .MODEL SMALL
 .STACK 100
 .DATA
 
-      include login.asm
-      include prodList.asm
+      include loginM~1.asm
 	  include menu.asm
-	  ;include payment.asm
+	  include quanti~1.asm
+	  include payment.asm
 	  ;include summary.asm
-
+	  
 .CODE
 MAIN PROC
       MOV AX,@DATA
 	  MOV DS,AX
-
+	  
 	  CALL login_menu
 	  CALL HOMAINMENU
-
+	  CALL payment
+	  
 ENDPROGRAM:
       MOV AH,4CH
 	  INT 21H
-
+	  
 MAIN ENDP
 END MAIN
+
